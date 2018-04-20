c3ad5721af00a037e02eee8decf82f5749cf40b3
Ekaterina.Podkhaliuzina <Ekaterina.Podkhaliuzina@jetbrains.com>
2018-04-20 14:47:24 +0300

    add int i

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("New Line");
+
+        int i;
+        i = 10;
+        System.out.println("i + 1 = " + i);
     }
 }
